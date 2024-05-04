47afda784405248e6217b95db93ec18bdb62097b
김경수 <galtab0403@gmail.com>
2024-05-04 15:17:31 +0900

    주석 제거

--- Assets/Scripts/Characters/Move/SimpleCharacterMove.cs
+++ Assets/Scripts/Characters/Move/SimpleCharacterMove.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//장애물이 있는 경로는 이동할 수 없는 MoveType 구현 클래스
+
 public class SimpleCharacterMove : AbstractMove
 {
     [SerializeField] 
@@ -11,7 +11,7 @@ public class SimpleCharacterMove : AbstractMove
     
     public override bool Move()
     {
-        if (bhasTarget())
+        if (!bhasTarget())
         {
             FindTarget();
             return false;
--- Assets/Scripts/Characters/Move/SimpleEnemyMove.cs
+++ Assets/Scripts/Characters/Move/SimpleEnemyMove.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//장애물이 있는 경로는 이동할 수 없는 MoveType 구현 클래스
+
 public class SimpleEnemyMove : AbstractMove
 {
     [SerializeField] 
@@ -11,7 +11,7 @@ public class SimpleEnemyMove : AbstractMove
     
     public override bool Move()
     {
-        if (bhasTarget())
+        if (!bhasTarget())
         {
             FindTarget();
             return false;
