0e427f8f39e2ab2f258b2e5f622e83b27147c30a
김경수 <galtab0403@gmail.com>
2024-05-14 17:03:18 +0900

    implement hpbar

---
+++ Assets/Scripts/UI/WorldObject/GameScene/HPBar.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : UI_Base
+{
+    private BaseCharacter _character;
+    private Stat _stat;
+    [SerializeField] private float _visibleTime = 1f;
+    enum GameObjects
+    {
+        HPBar,
+    }
+    public override void Init()
+    {
+        Bind<GameObject>(typeof(GameObjects));
+    }
+
+    private void Start()
+    {
+        _character = transform.parent.GetComponent<BaseCharacter>();
+        _stat = transform.parent.GetComponent<Stat>();
+        _character.HurtEvent -= UpdateHpBar;
+        _character.HurtEvent += UpdateHpBar;
+    }
+
+    private Coroutine _coroutine;
+    public void UpdateHpBar()
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+        _coroutine = StartCoroutine(OnGameObjectVisible());
+        float ratio = _stat.Hp / _stat.MaxHp;
+        setHpRatio(ratio);
+    }
+
+    public void DisableHpBar()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void setHpRatio(float ratio)
+    {
+        if (ratio < 0)
+            ratio = 0;
+        if (ratio > 1)
+            ratio = 1;
+        GetObject((int)GameObjects.HPBar).GetComponent<Slider>().value = ratio;
+    }
+
+    IEnumerator OnGameObjectVisible()
+    {
+        this.gameObject.SetActive(true);
+        yield return new WaitForSeconds(_visibleTime);
+        DisableHpBar();
+    }
+}
